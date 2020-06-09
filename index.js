fd172c73b2e196c48742af7461e3611ffe6e4470
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-06-09 17:02:47 -0400

    get, get by id, delete, and post are working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
 const server = express()
 
-const postRouter = require('.api/posts/')
+const postRouter = require('./posts/postRouter')
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
-server.use('/post/users', postRouter)
+server.use('/api/posts', postRouter)
 
 server.listen(8001, () => console.log('Sever Running on Port 8001'))
+
+
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,31 +1,86 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const db = require('./postDb')
+const db = require("./postDb");
 
-
-// ****************************************************************** // 
-//GET Request
-router.get('/', (req, res) => {
+// ****************************************************************** //
+//GET Request ✅
+router.get("/", (req, res) => {
   db.get()
-    .then(response => {
-      res.status(200).json(response)
+    .then((response) => {
+      res.status(200).json(response);
     })
     .catch(() => {
-      console.log("Error / Working")
+      console.log("Error / Working");
+    });
+});
+
+//GET Request by :id ✅
+router.get("/:id", (req, res) => {
+  const postId = req.params.id;
+
+  db.getById(postId)
+    .then((response) => {
+      if (response.length === 0) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json({ response });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Oops, something went wrong, please try again",
+      });
+    });
+});
+
+//DELETE by :id ✅
+router.delete("/:id", (req, res) => {
+  const postId = req.params.id;
+
+  db.remove(postId)
+    .then(response => {
+      if(response === 0 ) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        })
+      } else {
+        res.status(200).json(response)
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message:"Oops! Something went wrong, please try again."
+      })
     })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
+//PUT by :id
+router.put("/:id", (req, res) => {
+  const postId = req.params.id
+  const postBody = req.body
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(postId, postBody)
+    .then(response => {
+      if(response === 0) {
+        res.status(404).json({
+          message:`The user id entered ${postId} does not exist`
+        })
+      } else if(!postBody.text) {
+        res.status(400).json({
+          message:"Please prove a text field"
+        })
+      } else {
+          res.status(200).json({response})
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message:"Oops! Something went wrong, please try again."
+      })
+    })
 });
 
 // custom middleware
